6f8c409e1b1425c74d55682fb3aec29b5cd07413
Dr Terry Ramabulana <terry@tisholdings.co.za>
2025-09-29 22:26:54 +0200

    feat: router + ISO50001 route; SPA rewrites for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-﻿import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import ExecutiveDashboardPage from './ExecutiveDashboardPage';
-import ISO50001Assessment from './ISO50001Assessment';
-import ESGIntegrationDashboard from './ESGIntegrationDashboard';
-import ClientInformationForm from './ClientInformationForm';
-import APIKeyManagement from './APIKeyManagement';
+﻿import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ExecutiveDashboardPage from "./ExecutiveDashboardPage";
+import ISO50001Assessment from "./ISO50001Assessment.jsx";
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <>
+      <nav style={{padding:12, borderBottom:"1px solid #eee"}}>
+        <Link to="/" style={{marginRight:12}}>Dashboard</Link>
+        <Link to="/iso50001">ISO 50001 Assessment</Link>
+      </nav>
+
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<ExecutiveDashboardPage />} />
+        <Route path="/" element={<ExecutiveDashboardPage />} />
         <Route path="/iso50001" element={<ISO50001Assessment />} />
-        <Route path="/integrations" element={<ESGIntegrationDashboard />} />
-        <Route path="/client-form" element={<ClientInformationForm />} />
-        <Route path="/api-keys" element={<APIKeyManagement />} />
       </Routes>
-    </Router>
+    </>
   );
 }
-
-export default App;
