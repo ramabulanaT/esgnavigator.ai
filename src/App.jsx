74b560fe75884b2afb10ad4def59f4ad73dacd47
Terry Ramabulana <terry@tisholdings.co.za>
2025-10-06 12:12:24 +0200

    fix: remove imports for non-existent components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ExecutiveDashboardPage from './ExecutiveDashboardPage';
+import ISO50001Assessment from './ISO50001Assessment';
+import CSMTrainingLanding from './CSMTrainingLanding';
+import CSMDomainDetail from './CSMDomainDetail';
+import CSMEnrollmentForm from './CSMEnrollmentForm';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ExecutiveDashboardPage />} />
+        <Route path="/iso50001" element={<ISO50001Assessment />} />
+        <Route path="/training" element={<CSMTrainingLanding />} />
+        <Route path="/training/:domainId" element={<CSMDomainDetail />} />
+        <Route path="/training/enroll" element={<CSMEnrollmentForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;import LandingPage from './LandingPage';
 import ExecutiveDashboardPage from './ExecutiveDashboardPage';
 import ISO50001Assessment from './ISO50001Assessment';
 import ISO14001Assessment from './ISO14001Assessment';
