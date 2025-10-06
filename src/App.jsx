ade7b8c42474ba259c14a13148661c83f45f0c6b
Terry Ramabulana <terry@tisholdings.co.za>
2025-10-06 13:06:41 +0200

    fix: remove duplicate App declaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ExecutiveDashboardPage from './ExecutiveDashboardPage';
 import ISO50001Assessment from './ISO50001Assessment';
 import CSMTrainingLanding from './CSMTrainingLanding';
@@ -17,54 +15,6 @@ function App() {
         <Route path="/training" element={<CSMTrainingLanding />} />
         <Route path="/training/:domainId" element={<CSMDomainDetail />} />
         <Route path="/training/enroll" element={<CSMEnrollmentForm />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;import LandingPage from './LandingPage';
-import ExecutiveDashboardPage from './ExecutiveDashboardPage';
-import ISO50001Assessment from './ISO50001Assessment';
-import ISO14001Assessment from './ISO14001Assessment';
-import ISO45001Assessment from './ISO45001Assessment';
-import ESGIntegrationDashboard from './ESGIntegrationDashboard';
-import ClientInformationForm from './ClientInformationForm';
-import APIKeyManagement from './APIKeyManagement';
-import CSMTrainingLanding from './CSMTrainingLanding';
-import CSMDomainDetail from './CSMDomainDetail';
-import CSMEnrollmentForm from './CSMEnrollmentForm';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<ExecutiveDashboardPage />} />
-        <Route path="/iso50001" element={<ISO50001Assessment />} />
-        <Route path="/iso14001" element={<ISO14001Assessment />} />
-        <Route path="/iso45001" element={<ISO45001Assessment />} />
-        <Route path="/integrations" element={<ESGIntegrationDashboard />} />
-        <Route path="/client-form" element={<ClientInformationForm />} />
-        <Route path="/api-keys" element={<APIKeyManagement />} />
-        <Route path="/training" element={<CSMTrainingLanding />} />
-        <Route path="/training/:domainId" element={<CSMDomainDetail />} />
-        <Route path="/training/enroll" element={<CSMEnrollmentForm />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ExecutiveDashboardPage from './ExecutiveDashboardPage';
-import ISO50001Assessment from './ISO50001Assessment';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ExecutiveDashboardPage />} />
-        <Route path="/iso50001" element={<ISO50001Assessment />} />
       </Routes>
     </Router>
   );
